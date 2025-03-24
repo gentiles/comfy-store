ed0d69d56ba7e6846ab64cd638ba09448ead4554
USHM <erwinmarin05@gmail.com>
2025-03-25 00:29:01 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-7xl font-bold underline">Tailwind project</h1>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/*eslint-env node*/
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Adjust paths as needed
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
