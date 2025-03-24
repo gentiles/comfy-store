790845f3adf68e9e879b6bc209eb3e2680b6a97b
USHM <erwinmarin05@gmail.com>
2025-03-25 06:12:12 +0800

    'pagesbuilding'

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /*eslint-env node*/
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Adjust paths as needed
+  content: ["./src/**/*.{html,js,jsx,ts,tsx,d}"], // Adjust paths as needed
   theme: {
     extend: {},
   },
